221c8d89b324290a2c4ed9b05a05646cabd870c4
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-04 19:11:21 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# LEGENDBOT Assistant
 from . import *
 from telethon import Button, custom
 
@@ -12,6 +13,12 @@ LEGEND_USER = bot.me.first_name
 Its_LegendBoy = bot.uid
 
 legend_mention = f"[{LEGEND_USER}](tg://user?id={Its_LegendBoy})"
+LEGEND_logo = "./userbot/resources/pics/-6163428037589314866_121.jpg"
+LEGEND_logo1 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+LEGEND_logo2 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+LEGEND_logo4 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+LEGEND_logo3 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+LEGENDversion = "𝚅2.1"
 
 perf = "[ †hê Lêɠêɳ̃dẞø† ]"
 
